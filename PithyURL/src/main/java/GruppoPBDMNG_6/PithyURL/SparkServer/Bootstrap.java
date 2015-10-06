c1da241348f6827ef25d39519123032f0ea9c341
guerragiuseppe1507 <Evangelion92>
2015-10-06 21:45:30 +0200

    modifica pom

--- PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
+++ PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
@@ -11,7 +11,7 @@ import static spark.SparkBase.staticFileLocation;
  
 public class Bootstrap {
     private static final String IP_ADDRESS = "localhost";
-    private static final int PORT = 8080;
+    private static final int PORT = 8081;
  
     public static void main(String[] args) throws Exception {
         setIpAddress(IP_ADDRESS);
