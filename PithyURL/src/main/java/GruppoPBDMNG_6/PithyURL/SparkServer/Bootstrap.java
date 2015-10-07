4b54c4c159c2ef919f79ef2c0235cd6b11682239
guerragiuseppe1507 <Evangelion92>
2015-10-07 12:41:40 +0200

    Spostati i test nelle cartelle giuste per eseguirli con mvn test

--- PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
+++ PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
@@ -5,16 +5,16 @@ import GruppoPBDMNG_6.PithyURL.DataAccess.MongoDBDAO;
 
 import com.mongodb.*;
 
-import static spark.Spark.setIpAddress;
+//import static spark.Spark.setIpAddress;
 import static spark.Spark.setPort;
 import static spark.SparkBase.staticFileLocation;
  
 public class Bootstrap {
-    private static final String IP_ADDRESS = "192.168.1.3";
+    //private static final String IP_ADDRESS = "192.168.1.3";
     private static final int PORT = 8081;
  
     public static void main(String[] args) throws Exception {
-        setIpAddress(IP_ADDRESS);
+        //setIpAddress(IP_ADDRESS);
         setPort(PORT);
         staticFileLocation("/public");
         new Resource((IDAO) new MongoDBDAO(mongo()));
