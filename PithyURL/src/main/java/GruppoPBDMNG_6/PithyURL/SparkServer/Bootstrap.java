526aca322de08fe920855f81ffe8db772821d111
guerragiuseppe1507 <Evangelion92>
2015-10-07 09:27:11 +0200

    fix : rimosso setIpAddress('localhost') di spark perchè non funziona nella vm quando fai build del dockerfile

--- PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
+++ PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
@@ -10,11 +10,11 @@ import static spark.Spark.setPort;
 import static spark.SparkBase.staticFileLocation;
  
 public class Bootstrap {
-    private static final String IP_ADDRESS = "localhost";
+    //private static final String IP_ADDRESS = "localhost";
     private static final int PORT = 8080;
  
     public static void main(String[] args) throws Exception {
-        setIpAddress(IP_ADDRESS);
+        //setIpAddress(IP_ADDRESS);
         setPort(PORT);
         staticFileLocation("/public");
         new Resource((IDAO) new MongoDBDAO(mongo()));
