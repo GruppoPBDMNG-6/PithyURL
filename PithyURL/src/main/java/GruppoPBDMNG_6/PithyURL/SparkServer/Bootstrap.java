81400caf690fa61f8df493f1d24fc80211d27aee
guerragiuseppe1507 <Evangelion92>
2015-10-08 04:35:11 +0200

    fix pom

--- PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
+++ PithyURL/src/main/java/GruppoPBDMNG_6/PithyURL/SparkServer/Bootstrap.java
@@ -11,7 +11,7 @@ import static spark.SparkBase.staticFileLocation;
  
 public class Bootstrap {
     //private static final String IP_ADDRESS = "192.168.1.3";
-    private static final int PORT = 8082;
+    private static final int PORT = 8080;
  
     public static void main(String[] args) throws Exception {
         //setIpAddress(IP_ADDRESS);
---
+++ PithyURL/target/classes/public/js/controllers/main.js
@@ -0,0 +1,98 @@
+app.controller('CreateCtrl', function ($scope, $rootScope, $http, $location) {
+	
+	$scope.mainView = mainView;
+	
+	$scope.hostname = host;
+	
+    $scope.lsurl = {};
+    
+    $rootScope.response = false;
+    $rootScope.error = false;
+    
+    $rootScope.errorS = false;
+    
+    $scope.textToCopy = 'I can copy by clicking!';
+
+	$scope.createLsUrl = function() {
+		console.log("ciao");
+		$scope.lsurl.custom = false;
+		$scope.lsurl.short = "not_set";
+		$rootScope.response = false;
+		console.log($scope.lsurl);
+		$http.post('/api/v1/lsurl', $scope.lsurl).success(function(data) {
+			$location.path('/');
+			console.log(data.id);
+			$scope.createdUrl = data;
+			$rootScope.response = true;
+			$rootScope.error = false;
+		}).error(function(data, status) {
+			console.log('Error ' + data);
+			if (status == 502){
+				$scope.textError = "Please insert a real URL.";
+			}
+			$rootScope.response = true;
+			$rootScope.error = true;
+		})
+	}
+
+	$scope.createLsUrlCustom = function() {
+		console.log("ciao");
+		$scope.lsurl.custom = true;
+		console.log($scope.lsurl);
+		$rootScope.response = false;
+		$http.post('/api/v1/lsurl', $scope.lsurl).success(function(data) {
+			$location.path('/');
+			$scope.createdUrl = data;
+			$rootScope.response = true;
+			$rootScope.error = false;
+		}).error(function(data, status) {
+			console.log('Error ' + data);
+			if(status == 500){
+				$scope.textError = "Short URL contains unaccettable words!";
+			}else if (status == 502){
+				$scope.textError = "Please insert a real URL.";
+			}else if (status == 503){
+				$scope.textError = "Short URL already exists!";
+			}else if (status == 504){
+				$scope.textError = "Short URL must have at least 32 characters!";
+			}
+			$rootScope.response = true;
+			$rootScope.error = true;})
+	}
+
+	$scope.toStats = function() {
+		mainView = false;
+		$scope.mainView = mainView;
+		$scope.resetMsgs();
+		$(".nav-stats").addClass("current-nav");
+		$(".nav-home").removeClass("current-nav");
+	}
+	
+	$scope.toMain = function() {
+		mainView = true;
+		$scope.mainView = mainView;
+		$scope.resetMsgs();
+		$(".nav-stats").removeClass("current-nav");
+		$(".nav-home").addClass("current-nav");
+	}
+	
+	$scope.resetMsgs = function() {
+		$rootScope.response = false;
+	    $rootScope.error = false;
+	    $rootScope.errorS = false;
+	    $scope.lsurl.short = null;
+	}
+	 
+    $scope.success = function () {
+        console.log('Copied!');
+    };
+
+    $scope.fail = function (err) {
+        console.error('Error!', err);
+    };
+    
+    $scope.saveShort = function () {
+    	$scope.textToCopy = $("#text-to-copy").text();
+    };
+
+});
